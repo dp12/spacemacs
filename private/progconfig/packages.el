1cc43f7d4f75760e172f44fe86f6c6cc89390948
dp12 <deep.paren.12@gmail.com>
2015-06-03 12:15:03 -0400

    Replaced comment-dwim with custom comment toggler.

--- private/progconfig/packages.el
+++ private/progconfig/packages.el
@@ -68,6 +68,19 @@ which require an initialization must be listed explicitly in the list.")
         try-expand-dabbrev-all-buffers
         try-expand-dabbrev-from-kill
         try-expand-tag))
+
+; Comment toggle
+(defun comment-or-uncomment-region-or-line ()
+  "Comments or uncomments the region or the current line if there's no active region."
+  (interactive)
+  (let (beg end)
+    (if (region-active-p)
+        (setq beg (region-beginning) end (region-end))
+      (setq beg (line-beginning-position) end (line-end-position)))
+    (comment-or-uncomment-region beg end)
+    (next-line)))
+(global-set-key (kbd "M-;") 'comment-or-uncomment-region-or-line)
+(global-set-key (kbd "C-M-;") 'comment-dwim)
 ;;
 ;; Often the body of an initialize function uses `use-package'
 ;; For more info on `use-package', see readme:
