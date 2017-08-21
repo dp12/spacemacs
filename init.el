34f225565bb5a02b752f7f3e0e9c8e51cf5ad8b0
dp12 <deep.paren.12@gmail.com>
2017-08-21 11:56:51 -0400

    Make fixes to wdired and xwidget mode.

--- init.el
+++ init.el
@@ -509,6 +509,10 @@ lines downward first."
        (setq dired-listing-switches "--group-directories-first -alh")
        ))
 
+  ;; Start wdired in evil normal mode
+  (add-hook 'wdired-mode-hook
+            (lambda () (evil-normal-state)))
+
   (defun kill-dired-buffers()
     (interactive)
     (mapc (lambda (buffer)
@@ -570,8 +574,8 @@ lines downward first."
   (setq ivy-re-builders-alist
         '(
           ;; (counsel-imenu . ivy--regex-fuzzy)
-          ;; (counsel-bookmark . ivy--regex-fuzzy)
-          ;; (ivy-switch-buffer . ivy--regex-fuzzy)
+          (counsel-bookmark . ivy--regex-fuzzy)
+          (ivy-switch-buffer . ivy--regex-fuzzy)
           (t . ivy--regex-plus)))
   ;; automatically start looking for functions in counsel imenu
   (setq ivy-initial-inputs-alist '((counsel-imenu . "Functions: ")))
@@ -620,7 +624,7 @@ lines downward first."
     "rg" 'projectile-ripgrep
     "rc" 'counsel-rg
     "ir" 'ivy-resume
-    "ry" 'helm-show-kill-ring
+    "ry" 'counsel-yank-pop
     "bj" 'counsel-bookmark
     "ib" 'counsel-bookmark
     "ix" 'ido-clear-virtual-buffers
@@ -756,8 +760,14 @@ lines downward first."
     (define-key xwidget-webkit-mode-map [mouse-5] 'xwidget-webkit-scroll-up)
     (define-key xwidget-webkit-mode-map (kbd "<up>") 'xwidget-webkit-scroll-down)
     (define-key xwidget-webkit-mode-map (kbd "<down>") 'xwidget-webkit-scroll-up)
+    (define-key xwidget-webkit-mode-map (kbd "k") 'xwidget-webkit-scroll-down)
+    (define-key xwidget-webkit-mode-map (kbd "j") 'xwidget-webkit-scroll-up)
     (define-key xwidget-webkit-mode-map (kbd "M-w") 'xwidget-webkit-copy-selection-as-kill)
     (define-key xwidget-webkit-mode-map (kbd "C-c") 'xwidget-webkit-copy-selection-as-kill))
+  (add-hook 'xwidget-webkit-mode-hook
+            (lambda ()
+              (define-key evil-motion-state-map "k" 'xwidget-webkit-scroll-down)
+              (define-key evil-motion-state-map "j" 'xwidget-webkit-scroll-up)))
 
   ;; adapt webkit according to window configuration chagne automatically
   ;; without this hook, every time you change your window configuration,
