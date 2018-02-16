52bab2d16b8429e929518552df1cfabc0a52010b
dp12 <deep.paren.12@gmail.com>
2018-02-16 15:07:40 -0500

    Relaunch emacs in doom-switcher.sh

--- doom-switcher.sh
+++ doom-switcher.sh
@@ -5,12 +5,14 @@ if [[ -d ~/.emacs.d.spacemacs ]]; then
     pkill emacs
     mv ~/.emacs.d ~/.emacs.d.doom
     mv ~/.emacs.d.spacemacs ~/.emacs.d
+    bspc rule -a Emacs desktop=^1 state=tiled -o; nohup emacs >/dev/null 2>&1 &
 elif [[ -d ~/.emacs.d.doom || -f ~/.emacs.d/spacemacs.mk ]]; then
     echo "Switch to doom"
     pkill emacs
     pkill emacs
     mv ~/.emacs.d ~/.emacs.d.spacemacs
     mv ~/.emacs.d.doom ~/.emacs.d
+    bspc rule -a Emacs desktop=^1 state=tiled -o; nohup emacs >/dev/null 2>&1 &
 else
     echo "No cached spacemacs or doom configurations exist"
 fi
