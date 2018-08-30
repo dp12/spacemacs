fa5d4b44d1fa8f40202970c1f3e6e2aff29bd852
dp12 <deep.paren.12@gmail.com>
2018-08-30 11:03:47 -0400

    Remove extraneous evil-briefcase initialization

--- layers/progconfig/packages.el
+++ layers/progconfig/packages.el
@@ -24,7 +24,6 @@
     company-lsp
     lsp-ui
     gxref
-    ;; evil-briefcase-mode
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
