342130646980cd1186df5df515da6610b56151a6
dp12 <deep.paren.12@gmail.com>
2018-10-11 17:17:21 -0400

    Disable ycmd (replaced by cquery)

--- init.el
+++ init.el
@@ -83,7 +83,7 @@ This function should only modify configuration layer settings."
      themes-megapack
      version-control
      vinegar
-     ycmd
+     ;; ycmd
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
