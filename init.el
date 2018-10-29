381babe44c39454e88e5687d30415276b6d4c711
dp12 <deep.paren.12@gmail.com>
2018-10-29 11:30:23 -0400

    Rebind ediff-copy-both-to-C

--- init.el
+++ init.el
@@ -750,8 +750,8 @@ lines downward first."
                      (concat
                       (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
                       (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
-  (defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
-  (add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
+  (defun add-c-to-ediff-mode-map () (define-key ediff-mode-map "c" 'ediff-copy-both-to-C))
+  (add-hook 'ediff-keymap-setup-hook 'add-c-to-ediff-mode-map)
 
   ;; Dired
   (with-eval-after-load 'dired
